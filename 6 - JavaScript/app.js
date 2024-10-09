afdb18f5ff0fff1e5d8c2f337b869575853829a9
AristonCatipay <aristoncatipay123@gmail.com>
2024-10-09 11:06:43 +0800

    Start the discussion for Javascript.

---
+++ 6 - JavaScript/app.js
@@ -0,0 +1,17 @@
+greet();
+greet = (num1, num2) => {
+    console.log("Hi!");
+    return num1 + num2;
+}
+console.log(greet(1,2));
+
+function greet (){
+ console.log("Hello!");
+}
+
+greet();
+let fruits = ["Banana", "Apple", "Cherry", "Mango"];
+console.log(fruits);
+fruits.splice(1, 1,);
+console.log("Updated Fruits:", fruits);
+
